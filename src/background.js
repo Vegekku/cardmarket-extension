f11db2ac1dd3d7be3eeb3eba22da50ef7bab7ef3
Vegekku <vegekku@gmail.com>
2026-08-10 02:00:57 +0200

    chore: remove unused background.js service worker

--- src/background.js
+++
@@ -1,5 +0,0 @@
-/**
- * @module background
- * @description Service worker de la extensión. content.js se carga automáticamente
- * vía content_scripts en manifest.json; no se requiere inyección manual.
- */
